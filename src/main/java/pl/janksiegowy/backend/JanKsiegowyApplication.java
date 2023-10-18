d5e8b1fb87cdf8f1908145ae884b30cfef027176
Piotr Korniak <piotr@korniak.pl>
2023-10-18 22:39:51 +0200

    Initial commit.

---
+++ src/main/java/pl/janksiegowy/backend/JanKsiegowyApplication.java
@@ -0,0 +1,13 @@
+package pl.janksiegowy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanKsiegowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanKsiegowyApplication.class, args);
+	}
+
+}
